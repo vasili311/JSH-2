a7bc19ca525d452e1d54cab6f470c9bf445be5cd
vasili311 <116735770+vasili311@users.noreply.github.com>
2022-11-26 13:54:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+//TASK1
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Task 2
+const user1 = {
+    name: "Vasili",
+    lastname: "Vakhtangishvili",
+  };
+  const copyNewUser = function (user) {
+    const newUser = { ...user };
+    return newUser;
+  };
+  const copiedUser = copyNewUser(user1);
+  console.log(copiedUser); //{name: 'Vasili', lastname: 'Vakhtangishvili'}
+  
+
+
+//Task 3 
+while (true) {
+    const firstplayer = Math.floor(Math.random() * 6 + 1);
+    const secondplayer = Math.floor(Math.random() * 6 + 1);
+    console.log(firstplayer);
+    console.log(secondplayer);
+    if (firstplayer === 3 && secondplayer === 3) {
+      console.log("Draw!");
+      break;
+    } else if (firstplayer === 3) {
+      console.log("FirstPlayer  won the game");
+      break;
+    } else if (secondplayer === 3) {
+      console.log("SecondPlayer won the game");
+      break;
+    }
+  }
