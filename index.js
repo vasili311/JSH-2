9bd2ed31208cd3a911ec641982b1c11862586455
vasili311 <116735770+vasili311@users.noreply.github.com>
2022-11-28 16:28:35 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-//TASK1
+// TASK 1:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const user2= [
+    {name:'Vasili',age: 21},
+    {name:'Luka', age:26},
+    {name: 'Giorgi', age: 31}
+]
+let minElement = 100;
+let name2;
+function min(user2){
+    for(let i = 0; i < user2.length; i++){
+        if(user2[i].age < minElement){
+            minElement = user2[i].age;
+            name2 = user2[i].name;
+        }
+    }
+    
+    console.log('Smallest: ' + name2 );
+}
+min(user2);
 
 // Task 2
 const user1 = {
